1ff30febb1e9d7da58d563f63781020b5097d0d1
billou882 <billou2522@gmail.com>
2026-08-25 22:55:13 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,29 @@
-// Configuration Firebase issue de votre projet "cache-cache-draveil"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBcxudeQK91giQA5kzSa6wnFZzJIgODjq8",
+  apiKey: "AIzaSyBcxudeQK91giQA5kzSa6wnFzZJIgODjq8",
   authDomain: "cache-cache-draveil.firebaseapp.com",
   databaseURL: "https://cache-cache-draveil-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "cache-cache-draveil",
   storageBucket: "cache-cache-draveil.firebasestorage.app",
   messagingSenderId: "809078029731",
-  appId: "1:809078029731:web:83e384a38ce01254016e16",
-  measurementId: "G-GGFG133013"
+  appId: "1:809078029731:web:83e384a38ce01254016e16"
 };
 
-// Initialisation de Firebase et de la Realtime Database pour l'application
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const db = getDatabase(firebaseApp);
+export const auth = getAuth(firebaseApp);
+export const storage = getStorage(firebaseApp);
+
+export async function connectFirebase() {
+  if (auth.currentUser) {
+    return auth.currentUser;
+  }
+
+  const credential = await signInAnonymously(auth);
+  return credential.user;
+}
